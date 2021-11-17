ddfa9ff0af8da1154e724d3d88d157ca630094d2
Andrej Čalfa <45832809+calfaand@users.noreply.github.com>
2021-11-17 08:48:55 +0100

    upd

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import pygame
 
-WIDTH, HEIGHT = 900, 900
+WIDTH, HEIGHT = 500, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Labyrint')
+
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
