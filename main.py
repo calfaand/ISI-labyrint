6a613b4aeb6046f048a6252baedcac5ab49f52de
Andrej Čalfa <45832809+calfaand@users.noreply.github.com>
2021-11-17 17:36:51 +0100

    upd

--- main.py
+++ main.py
@@ -7,34 +7,54 @@ WIDTH, HEIGHT = 500, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Labyrint')
 WHITE = (255, 255, 255)
+GREEN = (0,255,0)
 
-# class MAP:
-#     def __init__(self, map, tiles):
-#         self.tiles = pygame.image.load(tiles)
-#
-#         l = [line.strip() for line in open(map).readlines()]
-#         self.map = [[None]*len(l[0]) for j in range(len(l))]
-#         for i in range(len(l[0])):
-#             for j in range(len(l)):
-#                 print('l')
+W=20
+H=20
+MARGIN=5
+maps=[]
+WALL = pygame.image.load('assets/wall.png')
+
 
 def map():
+
     with open('maps/map1.txt', 'r') as f:
-        for line in f:
-            print (line)    #iba vypisuje do konzoly
-                    
-                
+        lines = f.readlines()
+        for line in lines:
+            maps.append(line.strip('\n').split(','))     # map je teraz 2d arr
+
+        # for line in map:
+        #     map.append(line.strip())
+
+
+
 
 FPS = 60
+display_surface = pygame.display.set_mode((500, 500 ))
+
+def map_in_gui():
+
+    for row in range(15):        #ze pocet rows
+        for col in range(15):     # pocet cols
+            if maps[row][col]==0:
+                break
+            display_surface.blit(WALL, (row*W, col*H))
+
+    pygame.display.flip()
+
+
+
 def draw_window():                          #vytvori bielu plochu a bude sa updatovat
     WIN.fill(WHITE)
-    map()
+    #map()
+    map_in_gui()
     pygame.display.update()
 
 
 def main():
     clock = pygame.time.Clock()
     run = True
+    map()
     while run:
         clock.tick(FPS) #max fps 60
         for event in pygame.event.get():
@@ -42,6 +62,7 @@ def main():
                 run = False
 
         draw_window()
+        map()
 
     pygame.quit()
 
