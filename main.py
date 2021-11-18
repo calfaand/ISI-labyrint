b97ab82fc8db2ec25866fd1d4f3f741951fe982d
Andrej Čalfa <45832809+calfaand@users.noreply.github.com>
2021-11-18 16:49:21 +0100

    upd

--- main.py
+++ main.py
@@ -2,26 +2,31 @@ import random
 
 import pygame
 import os
+import numpy as np
 
 WIDTH, HEIGHT = 500, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Labyrint')
 WHITE = (255, 255, 255)
 GREEN = (0,255,0)
-
+maps=[]
 W=20
 H=20
 MARGIN=5
-maps=[]
+
 WALL = pygame.image.load('assets/wall.png')
 
 
 def map():
+    # maps = np.genfromtxt('maps/map1.txt', dtype=None)
+    # print (maps)
 
     with open('maps/map1.txt', 'r') as f:
         lines = f.readlines()
         for line in lines:
-            maps.append(line.strip('\n').split(','))     # map je teraz 2d arr
+            maps.append(line.strip('\n').split(' '))# map je teraz 2d arr
+
+    print (maps)
 
         # for line in map:
         #     map.append(line.strip())
@@ -35,14 +40,13 @@ display_surface = pygame.display.set_mode((500, 500 ))
 
 
 
-
 def map_in_gui():
 
-    for row in range(15):        #ze pocet rows
-        for col in range(15):     # pocet cols
+    for row in range(1,10):        #ze pocet rows
+        for col in range(1,10):     # pocet cols
             if maps[row][col]==0:
                 break
-            display_surface.blit(WALL, (row*W, col*H))
+            display_surface.blit(WALL, (row*MARGIN,col*MARGIN))
 
     pygame.display.flip()
 
@@ -51,14 +55,19 @@ def map_in_gui():
 def draw_window():                          #vytvori bielu plochu a bude sa updatovat
     WIN.fill(WHITE)
     #map()
-    map_in_gui()
+    # map_in_gui()
+    # display_surface.blit(WALL, (1, 1))
+    # display_surface.blit(WALL, (30, 30))
+
     pygame.display.update()
 
 
 def main():
     clock = pygame.time.Clock()
+
     run = True
     map()
+    print(maps[1][1])
     while run:
         clock.tick(FPS) #max fps 60
         for event in pygame.event.get():
@@ -66,7 +75,7 @@ def main():
                 run = False
 
         draw_window()
-        map()
+
 
     pygame.quit()
 
