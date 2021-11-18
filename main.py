a3e65bc70ef7b9d24e972077828734e2219b2892
Andrej Čalfa <45832809+calfaand@users.noreply.github.com>
2021-11-18 19:52:52 +0100

    fungiruje vykreslenie mapy

--- main.py
+++ main.py
@@ -1,48 +1,40 @@
-import random
-
 import pygame
-import os
-import numpy as np
 
 WIDTH, HEIGHT = 500, 500
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Labyrint')
 WHITE = (255, 255, 255)
-GREEN = (0,255,0)
-maps=[]
-W=20
-H=20
-MARGIN=15
-
-
+GREEN = (0, 255, 0)
+maps = []
+W = 20
+H = 20
+MARGIN = 15
 FPS = 60
-display_surface = pygame.display.set_mode((500, 500 ))
+
+
 WALL = pygame.image.load('assets/wall.png')
+start_img = pygame.image.load('assets/start_btn.png').convert_alpha()
 
 
 def map():
-
     with open('maps/map1.txt', 'r') as f:
         lines = f.readlines()
         for line in lines:
-            maps.append(line.strip('\n').split(' '))# map je teraz 2d arr
+            maps.append(line.strip('\n').split(' '))  # map je teraz 2d arr
 
 
 def map_in_gui():
-
-    for row in range(len(maps)):        #ze pocet rows
-        for col in range(len(maps[0])):     # pocet cols
+    for row in range(len(maps)):  # ze pocet rows
+        for col in range(len(maps[0])):  # pocet cols
             if maps[row][col] == '0':
                 continue
-            display_surface.blit(WALL, (row*MARGIN, col*MARGIN))
-
+            screen.blit(WALL, (row * MARGIN, col * MARGIN))
 
     pygame.display.flip()
 
 
-
-def draw_window():                          #vytvori bielu plochu a bude sa updatovat
-    WIN.fill(WHITE)
+def draw_window():  # vytvori bielu plochu a bude sa updatovat
+    screen.fill(WHITE)
     #map()
     map_in_gui()
 
@@ -50,21 +42,24 @@ def draw_window():                          #vytvori bielu plochu a bude sa upda
 
 
 def main():
+
     clock = pygame.time.Clock()
     run = True
     map()
     while run:
-        clock.tick(FPS) #max fps 60
+
+
+        clock.tick(FPS)  # max fps 60
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:       #ohandlovanie X - zatvori okno
+            if event.type == pygame.QUIT:  # ohandlovanie X - zatvori okno
                 run = False
 
         draw_window()
 
-
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
 
-#dushan
+# dushan
