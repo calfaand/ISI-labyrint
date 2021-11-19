f59eba8fff5de0c3a3d2ad876f7ee75c773ebaa2
Andrej Čalfa <45832809+calfaand@users.noreply.github.com>
2021-11-19 12:43:47 +0100

    uvodne menu, nefunguje najlepsie ale aspon nieco tam je

--- main.py
+++ main.py
@@ -1,23 +1,67 @@
 import pygame
+from pygame.locals import *
 
+pygame.init()
+pygame.font.init()
 WIDTH, HEIGHT = 500, 500
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Labyrint')
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+GRAY = (50, 50, 50)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
 maps = []
 W = 20
 H = 20
 MARGIN = 15
 FPS = 60
-
-
+font = "Retro.ttf"
 WALL = pygame.image.load('assets/wall.png')
-start_img = pygame.image.load('assets/start_btn.png').convert_alpha()
+selected = 'start'
+
+
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+
+
+def text_format(message, textFont, textSize, textColor):
+    newFont = pygame.font.SysFont(textFont, textSize)
+    newText = newFont.render(message, 0, textColor)
+    return newText
+
+
+def menu():
+    screen.fill(BLUE)
+    title = text_format("Labyrint", font, 90, WHITE)
+    if selected == 'start':
+        text_start = text_format('START', font, 75, WHITE)
+    else:
+        text_start = text_format('START', font, 75, BLACK)
+    if selected == 'quit':
+        text_quit = text_format('QUIT', font, 75, WHITE)
+    else:
+        text_quit = text_format('QUIT', font, 75, BLACK)
+
+    title_rect = title.get_rect()
+    start_rect = text_start.get_rect()
+    quit_rect = text_quit.get_rect()
+
+    # menu text
+    screen.blit(title, (WIDTH / 2 - (title_rect[2] / 2), 80))
+    screen.blit(text_start, (WIDTH / 2 - (start_rect[2] / 2), 300))
+    screen.blit(text_quit, (WIDTH / 2 - (quit_rect[2] / 2), 360))
+
+    pygame.display.update()
 
 
 def map():
-    with open('maps/map1.txt', 'r') as f:
+    with open('maps/map2.txt', 'r') as f:
         lines = f.readlines()
         for line in lines:
             maps.append(line.strip('\n').split(' '))  # map je teraz 2d arr
@@ -35,26 +79,39 @@ def map_in_gui():
 
 def draw_window():  # vytvori bielu plochu a bude sa updatovat
     screen.fill(WHITE)
-    #map()
+    # map()
     map_in_gui()
 
     pygame.display.update()
 
 
 def main():
-
     clock = pygame.time.Clock()
     run = True
-    map()
+
+    # map()
     while run:
-
-
         clock.tick(FPS)  # max fps 60
         for event in pygame.event.get():
             if event.type == pygame.QUIT:  # ohandlovanie X - zatvori okno
                 run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    selected = 'start'
+                elif event.key == pygame.K_DOWN:
+                    selected = 'quit'
+                if event.key == pygame.K_RETURN:
+                    if selected == 'start':
+                        print('start')
+                        draw_window()
 
-        draw_window()
+                    if selected == 'quit':
+                        print('quit')
+                        pygame.quit()
+                        quit()
+
+        menu()
+        # draw_window()
 
     pygame.quit()
 
