a3ac015cba727a1e7c0f11c5404d46bab13571b6
Andrej Čalfa <45832809+calfaand@users.noreply.github.com>
2021-11-26 11:01:28 +0100

    upr dfs:55, treba cek string 0 v mape

--- dfs.py
+++ dfs.py
@@ -1,7 +1,6 @@
 from collections import deque
 import main
 
-
 class GridPosition:
     def __init__(self, x, y):
         self.x = x
@@ -54,7 +53,7 @@ def dfs(Grid, dest: GridPosition, start: GridPosition):
                 x_pos = curr_pos.x + adj_cell_x[i]
                 y_pos = curr_pos.y + adj_cell_y[i]
                 if x_pos != len(Grid) and x_pos !=-1 and y_pos != len(Grid[0]) and y_pos != -1:
-                    if Grid[x_pos][y_pos]==0:       #TODO: mozno treba dat tu 0 ..... 1 v GH ako volno, tuto 1 ako stena
+                    if Grid[x_pos][y_pos]=='0':       #TODO: mozno treba dat tu 0 ..... 1 v GH ako volno, tuto 1 ako stena
                         if not visited_blocks[x_pos][y_pos]:
                             cost+=1
                             visited_blocks[x_pos][y_pos] = True
